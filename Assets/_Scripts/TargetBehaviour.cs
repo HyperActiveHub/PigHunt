971f650fddd2a532e27e2ae47445ce275c648e97
Karim Chanane <karim.chanane.dev@gmail.com>
2023-01-19 17:00:25 +0100

    Added Target prefab

---
+++ Assets/_Scripts/TargetBehaviour.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetBehaviour : MonoBehaviour
+{
+	[SerializeField, Range(0.2f, 5f)] float animSpeedMultiplierMin = 0.5f;
+	[SerializeField, Range(0.2f, 5f)] float animSpeedMultiplierMax = 1.5f; 
+
+	Animator animator;
+	List<AnimatorControllerParameter> parameters;
+	string animSpeedParamName;
+    void Start()
+    {
+		animator = GetComponent<Animator>();
+		parameters = new List<AnimatorControllerParameter>(animator.parameters);
+
+		for(int i = 0; i < parameters.Count; i++)
+		{
+			if(parameters[i].type is AnimatorControllerParameterType.Float)
+			{
+				animSpeedParamName = parameters[i].name;
+				parameters.RemoveAt(i);
+				break;
+			}
+		}
+
+		var randomAnimIndex = Random.Range(0, parameters.Count);
+		animator.SetTrigger(parameters[randomAnimIndex].name);	//Set which movement animation to play
+
+		var randomSpeedMult = Random.Range(animSpeedMultiplierMin, animSpeedMultiplierMax);
+		animator.SetFloat(animSpeedParamName, randomSpeedMult);	//Set animation speed multiplier
+	}
+
+    void Update()
+    {
+		//Destroy target after its movement anination is done
+		if(animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !animator.IsInTransition(0))
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	public void TargetHit(Player player)
+	{
+		//KC: call score-system, was this target assigned to the player that hit?
+		//add points if player was assigned this target, or remove points if not
+		//Play Death-effect - confetti
+
+
+		Destroy(gameObject);
+		//print($"Player <{player.name}> hit target [{name}]!");
+	}
+
+
+}
