130bec4ed84dddb1d48ea378b405ad0c91f127a7
C4illin <emrik.ostling@gmail.com>
2023-01-20 12:29:29 +0100

    mainscreenfade

--- Assets/_Scripts/MainScreenFade.cs
+++ Assets/_Scripts/MainScreenFade.cs
@@ -1,18 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using static System.Net.Mime.MediaTypeNames;
 
-public class MainScreenFade : MonoBehaviour
+public class ImageFade : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    // the image you want to fade, assign in inspector
+    public Image img;
+
+    public void OnButtonClick()
     {
-        
+        // fades the image out when you click
+        StartCoroutine(FadeImage(true));
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator FadeImage(bool fadeAway)
     {
-        
+        // fade from opaque to transparent
+        if (fadeAway)
+        {
+            // loop over 1 second backwards
+            for (float i = 1; i >= 0; i -= Time.deltaTime)
+            {
+                // set color with i as alpha
+                img.color = new Color(1, 1, 1, i);
+                yield return null;
+            }
+        }
+        // fade from transparent to opaque
+        else
+        {
+            // loop over 1 second
+            for (float i = 0; i <= 1; i += Time.deltaTime)
+            {
+                // set color with i as alpha
+                img.color = new Color(1, 1, 1, i);
+                yield return null;
+            }
+        }
     }
 }
